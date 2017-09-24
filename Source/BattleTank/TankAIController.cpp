842dd25f7a474cf690d746ac97177b1160f962d6
lovethebomb <lucas@lovethebomb.eu>
2017-09-24 17:13:40 +0200

    Add Throttle basics

--- Source/BattleTank/TankAIController.cpp
+++ Source/BattleTank/TankAIController.cpp
@@ -21,6 +21,6 @@ void ATankAIController::Tick(float DeltaTime)
 		ControlledTank->AimAt(PlayerTank->GetActorLocation());
 
 		// Fire if ready
-		ControlledTank->Fire(); // TODO limit firing rate
+		ControlledTank->Fire(); 
 	}
 }
